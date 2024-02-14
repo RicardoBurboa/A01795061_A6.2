d948a1ad14189d7871ddd4f20f51b55f8ce1e3ad
Ricardo <39820053+RicardoBurboa@users.noreply.github.com>
2024-02-14 09:04:49 -0700

    Delete Problema1 directory

--- Problema1/main.py
+++
@@ -1,9 +0,0 @@
-from customer import Customer
-from hotel import Hotel
-from reservation import Reservation
-
-customer_instance = Customer()
-hotel_instance = Hotel()
-reservation_instance = Reservation()
-
-reservation_instance.cancel_reserv("8bfaabb5-3491-4df2-9eb3-3ffd1e2278dc")
